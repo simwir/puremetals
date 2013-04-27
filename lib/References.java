fa363e5b0eda7bad3719089167ae19199c394680
simwir <simwir@hotmail.com>
2013-04-27 22:55:15 +0300

    Update References.java

--- lib/References.java
+++ lib/References.java
@@ -3,5 +3,5 @@ package mods.simwir.puremetals.lib;
 public class References {
 	public static final String MOD_ID = "puremetals";
 	public static final String MOD_NAME = "Pure Metal";
-	public static final String MOD_VERSION = "0.0.2";
+	public static final String MOD_VERSION = "0.0.2.1";
 }
